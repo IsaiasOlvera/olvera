a027cc71ad11f0f75c588c29a39704b24295223b
Isolvera <31068725+Isolvera@users.noreply.github.com>
2017-08-26 22:57:15 -0500

    Add files via upload

---
+++ clase computadora.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of computadora
+ *
+ * @author olvera
+ */
+class computadora {
+    public modelo;
+	var precio;
+	var marca;
+	var descripcion;
+}
